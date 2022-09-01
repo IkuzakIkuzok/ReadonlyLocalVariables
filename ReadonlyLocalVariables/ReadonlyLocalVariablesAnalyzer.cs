f9d1073b965a6b82dd7ea9e2f6b63f692347e4cf
ikuzak <k.kohzuki@gmail.com>
2022-09-01 18:18:12 +0900

    Implemented basic functions.

---
+++ ReadonlyLocalVariables/ReadonlyLocalVariablesAnalyzer.cs
@@ -0,0 +1,98 @@
+﻿
+// (c) 2022 Kazuki KOHZUKI
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReadonlyLocalVariables
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ReadonlyLocalVariablesAnalyzer : DiagnosticAnalyzer
+    {
+        private static readonly DiagnosticDescriptor Rule = new(
+            "RO0001",
+            "Readonly local variable",
+            "Variable '{0}' is not allowed to be reassigned.",
+            "CodeStyle",
+            DiagnosticSeverity.Error,
+            true
+        );
+
+        override public ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+            => ImmutableArray.Create(Rule);
+
+        override public void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeNode,
+                SyntaxKind.AddAssignmentExpression,
+                SyntaxKind.AndAssignmentExpression,
+                SyntaxKind.CoalesceAssignmentExpression,
+                SyntaxKind.DivideAssignmentExpression,
+                SyntaxKind.ExclusiveOrAssignmentExpression,
+                SyntaxKind.LeftShiftAssignmentExpression,
+                SyntaxKind.ModuloAssignmentExpression,
+                SyntaxKind.MultiplyAssignmentExpression,
+                SyntaxKind.OrAssignmentExpression,
+                SyntaxKind.RightShiftAssignmentExpression,
+                SyntaxKind.SimpleAssignmentExpression,
+                SyntaxKind.SubtractAssignmentExpression
+            );
+        } // override public void Initialize (AnalysisContext)
+
+        private static async void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var semanticModel = context.SemanticModel;
+            if (semanticModel == null) return;
+
+            var node = (AssignmentExpressionSyntax)context.Node;
+            var leftNode = node.Left;
+            var leftSymbol = semanticModel.GetSymbolInfo(leftNode).Symbol;
+            if (leftSymbol == null) return;
+            var declaringSyntax = leftSymbol.OriginalDefinition.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax();
+            if (!CheckIfDeclarationIsLocal(declaringSyntax)) return;
+            var name = leftSymbol.Name ?? string.Empty;
+            if (await CheckMutableRulePatterns(node, name, context.CancellationToken)) return;
+            context.ReportDiagnostic(Diagnostic.Create(Rule, node.GetLocation(), name));
+        } // private static void AnalyzeNode (SyntaxNodeAnalysisContext)
+
+        private static bool CheckIfDeclarationIsLocal(SyntaxNode? declaringSyntax)
+        {
+            if (declaringSyntax?.Parent?.Parent is MemberDeclarationSyntax) return false;
+
+            return true;
+        } // private static bool CheckIfDeclarationIsLocal (SyntaxNode?)
+
+        private static async Task<bool> CheckMutableRulePatterns(SyntaxNode node, string name, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (node is MethodDeclarationSyntax methodDeclaration)
+            {
+                var attributes = methodDeclaration.AttributeLists;
+                foreach (var attribute in attributes.SelectMany(list => list.Attributes))
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    var attrName = attribute.Name.ToFullString();
+                    if (!attrName.EndsWith("Attribute")) attrName += "Attribute";
+                    if (attrName != "ReassignableVariableAttribute") continue;
+                    var args = attribute.ArgumentList?.Arguments ?? Enumerable.Empty<AttributeArgumentSyntax>();
+                    var names = args.Select(arg => arg.ToFullString()?.Trim('"'));
+                    if (names.Any(n => n == name)) return true;
+                }
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+            var parent = node.Parent;
+            if (parent == null) return false;
+            return await CheckMutableRulePatterns(parent, name, cancellationToken);
+        } // private static async Task<bool> CheckMutableRulePatterns (SyntaxNode, string, CancellationToken)
+    } // public class ReadonlyLocalVariablesAnalyzer : DiagnosticAnalyzer
+} // namespace ReadonlyLocalVariables
---
+++ ReadonlyLocalVariables/ReassignableVariableAttributeGenerator.cs
@@ -0,0 +1,50 @@
+﻿
+// (c) 2022 Kazuki KOHZUKI
+
+using Microsoft.CodeAnalysis;
+
+namespace ReadonlyLocalVariables
+{
+    [Generator]
+    public class ReassignableVariableAttributeGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        } // public void Initialize (GeneratorInitializationContext)
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource(
+                "__MutableVariablesRuleAttribute.cs",
+                @"
+using System;
+namespace ReadonlyLocalVariables
+{
+    /// <summary>
+    /// Specifies the name of identifier that is allowed to be reassigned within a scope.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    internal sealed class ReassignableVariableAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the variable identifiers.
+        /// </summary>
+        internal string[] Identifiers { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref=""ReassignableVariableAttribute""/> class
+        /// with the specified names. 
+        /// </summary>
+        /// <param name=""name"">The variable names.</param>
+        public ReassignableVariableAttribute(params string[] names)
+        {
+            this.Identifiers = names;
+        } // ctor (string)
+    } // internal sealed class ReassignableVariableAttribute : Attribute
+} // namespace ReadonlyLocalVariables
+"
+            );
+        } // public void Execute (GeneratorExecutionContext)
+    } // public class ReassignableVariableAttributeGenerator : ISourceGenerator
+} // namespace ReadonlyLocalVariables
