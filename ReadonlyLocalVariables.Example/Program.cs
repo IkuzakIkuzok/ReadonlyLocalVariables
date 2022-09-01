e1f401c4c3afc0a620311ab1e70f1014f234f033
ikuzak <k.kohzuki@gmail.com>
2022-09-01 18:26:07 +0900

    Added example.

---
+++ ReadonlyLocalVariables.Example/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// (c) 2022 Kazuki KOHZUKI
+
+using ReadonlyLocalVariables;
+using System;
+
+class C
+{
+    private static int Field = 0;
+
+    [ReassignableVariable("Local")]  // Explicitly state which local variables are allowed to be reassigned.
+    static void Main(string[] args)
+    {
+        var local = 0;
+        var Local = 0;
+
+        Console.WriteLine(local);
+        Console.WriteLine(Local);
+        Console.WriteLine(Field);
+
+        local = 1;  // Local variables are treated as read-only.
+        Local = 1;  // Local variable allowed to be reassigned.
+        Field = 1;  // Class members are not inspected because they have the `readonly` keyword.
+
+        Console.WriteLine(local);
+        Console.WriteLine(Local);
+        Console.WriteLine(Field);
+    }
+}
